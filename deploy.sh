bef93f762c87fd72d44831cc62a7449ec22d5edb
Bruno de Rezende <brunoderezende@hotmail.com>
2020-04-14 20:43:11 -0300

    EKS deploy attempt 7

--- deploy.sh
+++ deploy.sh
@@ -4,10 +4,10 @@ set -e
 
 if [ -z "$KUBECONFIG" ]
 then
-    echo ${KUBERNETES_CA} | base64 --decode > flash-tube-ca.pem
-    echo ${KUBERNETES_CLIENT_CA} | base64 --decode > flash-tube-client-ca.pem
-    echo ${KUBERNETES_CLIENT_KEY} | base64 --decode > flash-tube-key.pem
-    kubectl config set-cluster flash-tube --server=${KUBERNETES_ENDPOINT} --certificate-authority=flash-tube-ca.pem
+    echo "${KUBERNETES_CA}" > flash-tube-ca.pem
+    echo "${KUBERNETES_CLIENT_CA}" > flash-tube-client-ca.pem
+    echo "${KUBERNETES_CLIENT_KEY}" > flash-tube-key.pem
+    kubectl config set-cluster flash-tube --server="${KUBERNETES_ENDPOINT}" --certificate-authority=flash-tube-ca.pem
     kubectl config set-credentials kubernetes-admin --client-certificate=flash-tube-client-ca.pem --client-key=flash-tube-key.pem
     kubectl config set-context kubernetes-admin@flash-tube --cluster=flash-tube --namespace=default --user=kubernetes-admin
     kubectl config use-context kubernetes-admin@flash-tube
