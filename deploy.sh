768913cd2026fd16622beaf69a842cd0008ebe48
Bruno de Rezende <brunoderezende@hotmail.com>
2020-04-15 20:20:13 -0300

    GCP deploy 1

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,5 @@
 #!/bin/bash
 
-set -e
-
-if [ -z "$KUBECONFIG" ]
-then
-    echo "${KUBERNETES_CA}" > flash-tube-ca.pem
-    echo "${KUBERNETES_CLIENT_CA}" > flash-tube-client-ca.pem
-    echo "${KUBERNETES_CLIENT_KEY}" > flash-tube-key.pem
-    kubectl config set-cluster flash-tube --server="${KUBERNETES_ENDPOINT}" --certificate-authority=flash-tube-ca.pem
-    kubectl config set-credentials kubernetes-admin --client-certificate=flash-tube-client-ca.pem --client-key=flash-tube-key.pem
-    kubectl config set-context kubernetes-admin@flash-tube --cluster=flash-tube --namespace=default --user=kubernetes-admin
-    kubectl config use-context kubernetes-admin@flash-tube
-fi
-
 docker build -t brurez/flash-tube-client:latest -t brurez/flash-tube-client:"$SHA" -f ./client/Dockerfile ./client
 docker build -t brurez/flash-tube-server:latest -t brurez/flash-tube-server:"$SHA" -f ./server/Dockerfile ./server
 
